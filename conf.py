170aadbd35b8d7610024da82fa2d38857eef309a
Theo Lemay <theol0403@gmail.com>
2020-07-11 21:20:15 -0700

    Switch to python config

---
+++ conf.py
@@ -0,0 +1,13 @@
+DOXYFILE = "Doxyfile-mcss"
+
+FAVICON = "logo.png"
+THEME_COLOR = "#e6b85c"
+SHOW_UNDOCUMENTED = True
+
+STYLESHEETS = [
+    "./m.css/css/m-dark+documentation.compiled.css",
+    "https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,600i%7CSource+Code+Pro:400,400i,600",
+    "pros-custom.css",
+]
+
+LINKS_NAVBAR1 = [("Tutorials", "md_docs_tutorials_index", [])]
