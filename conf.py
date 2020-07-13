442b7421095ec87e9f0700516815fcb0d457236c
Theo Lemay <theol0403@gmail.com>
2020-07-12 21:58:06 -0700

    Add syntax hl

--- conf.py
+++ conf.py
@@ -8,6 +8,7 @@ STYLESHEETS = [
     "./m.css/css/m-dark+documentation.compiled.css",
     "https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,600i%7CSource+Code+Pro:400,400i,600",
     "pros-custom.css",
+    "pros-code.css",
 ]
 
 LINKS_NAVBAR1 = [("Tutorials", "md_docs_tutorials_index", [])]
